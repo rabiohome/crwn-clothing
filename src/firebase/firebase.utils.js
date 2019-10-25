ee720712af87763849b1b04efedc0941b86ccc07
rabiohome <rrahmouni.me@gmail.com>
2019-10-25 18:22:50 +0100

    integrated firebase into application, created sign in page with sign in component and sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyA_crdouwmNtM436vrbpULJTZTuliWu63Q",
+    authDomain: "crwn-db-45e9b.firebaseapp.com",
+    databaseURL: "https://crwn-db-45e9b.firebaseio.com",
+    projectId: "crwn-db-45e9b",
+    storageBucket: "crwn-db-45e9b.appspot.com",
+    messagingSenderId: "357164865146",
+    appId: "1:357164865146:web:fa75c015f596aa20e30dc3"
+  };
+  
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt : 'select_account' });
+  export const SignInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
