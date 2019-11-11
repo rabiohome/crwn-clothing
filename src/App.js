4e9e174ab839813167c42861df58c32e17316904
rabiohome <rrahmouni.me@gmail.com>
2019-11-11 12:10:11 +0100

    added checkout and checkout item components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCurrentUser } from './redux/user/user-selectors';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Checkout from './pages/checkout/checkout.component';
+
+import Header from './components/header/header.component';
 
 import { setCurrentUser } from './redux/user/user-actions';
+import { selectCurrentUser } from './redux/user/user-selectors';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -53,6 +56,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/"  component={HomePage} />
           <Route path="/shop"  component={ShopPage} />
+          <Route exact path="/checkout"  component={Checkout} />
           <Route path="/signin"  render={() => this.props.currentUser ? (<Redirect to="/"/>) : (<SignInAndSignUpPage/>)} />
         </Switch>
       </div>
