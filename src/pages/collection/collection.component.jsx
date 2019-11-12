c09900b682c1d69e5ee1869b5252f7fc1520cb73
rabiohome <rrahmouni.me@gmail.com>
2019-11-12 14:58:46 +0100

    converted to object our SHOP_DATA and achanged related reducers and components

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop-selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
-    <div className="collection-page">
-        <h2>COLLECTION PAGE : {match.params.collectionId}</h2>
-    </div>
-);
+const CollectionPage = ({ collection }) => {
 
-export default CollectionPage;
+    const { title, items } = collection;
+
+    return (
+        <div className="collection-page">
+            <h2>{ title }</h2>
+            <div className="items">
+                {
+                    items.map(item => 
+                    (<CollectionItem key={item.id} item={item} />)
+                )
+                }
+            </div>
+        </div>
+    );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+    collection : selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
