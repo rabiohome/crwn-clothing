4cd3c9ad8721b3e915a337c577880820b0f4c1cc
rabiohome <rrahmouni.me@gmail.com>
2019-11-13 11:53:48 +0100

    updated checkout component, added stripe button

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = "pk_test_MEVUzCPRC9liAx7k3qkYvRa200vYYufM6k";
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label="Pay Now"
+            name="CRWN Clothing Ltd."
+            billingAddress
+            shippingAddress
+            image="https://svgshare.com/i/CUz.svg"
+            description={`Your total price is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
